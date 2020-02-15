f167f985b3299945c2f013c7d47227b0f7a09b18
girlsways <109319858+girlsways@users.noreply.github.com>
2020-02-15 15:58:30 +0000

    Change back

--- demo/demo/app/admin.py
+++ demo/demo/app/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .model
+from .models import User
+
+
+def _get_email_field():
+    return User.EMAIL_FI
