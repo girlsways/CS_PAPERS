5e68c793b1cfbe8a3250240113745c93e2c780e1
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-09 19:12:24 +0000

    Updating pages from dalekjs.com

--- demo/demo/app/factories.py
+++ demo/demo/app/factories.py
@@ -2,4 +2,9 @@ from factory import Faker, LazyAttribute, SubFactory
 from factory.django import DjangoModelFactory
 
 from django_graphql_jwt_flow.models import JwtRefreshToken
-from .models import
+from .models import User
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = U
