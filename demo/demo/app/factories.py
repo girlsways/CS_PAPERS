e0df4acf6c96bbd120e10f9b876275606694e643
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-11 20:54:40 +0000

    set travisdir using -bash so that it works on OS X too

--- demo/demo/app/factories.py
+++ demo/demo/app/factories.py
@@ -20,4 +20,8 @@ class UserFactory(DjangoModelFactory):
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
         manager = cls._get_manager(model_class)
-        return manager.cre
+        return manager.create_user(*args, **kwargs)
+
+
+class JwtRefreshTokenFactory(DjangoModelFactory):
+    cl
