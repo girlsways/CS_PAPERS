5c78ee8209ff5dcd77445dc2efe386c60f1bbe15
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-12 18:07:42 +0000

    Linux 3.10.47

--- demo/demo/asgi.py
+++ demo/demo/asgi.py
@@ -1,4 +1,7 @@
 """
 ASGI config for demo project.
 
-It exposes the ASGI callable as a module-level va
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.dj
