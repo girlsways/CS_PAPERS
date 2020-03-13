019ac3b1db4c4d49cd7f707cea6826bef09d854a
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-13 20:31:06 +0000

    fix README.md

--- demo/demo/env_utils.py
+++ demo/demo/env_utils.py
@@ -7,4 +7,5 @@ def get_list(name, separator=":") -> t.List[str]:
     return os.getenv(name, "").split(separator)
 
 
-def yesno(name, default
+def yesno(name, default: str = "no") -> bool:
+    return os.getenv(name, default).lower() in [
