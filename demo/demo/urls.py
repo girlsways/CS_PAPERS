b8f3b2ab9c4baefe3175181ff4752d36763a24a8
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-19 17:43:23 +0000

    alpha: fix fpu.h usage in userspace

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -5,4 +5,6 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.h
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_a
