a8c8d4cfa0b4c48bd4ca91712f2d1cab3e1c8d87
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-19 15:43:42 +0000

    rebased

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -4,4 +4,5 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from m
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.h
