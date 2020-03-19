ae95cd081bbe9c9c755a49edbe21de5b6a96f25b
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-19 19:07:35 +0000

    typo bugfix

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -7,4 +7,5 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_a
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('',
