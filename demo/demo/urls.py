e4a7a60d2d779c8cc273ec0682e3874c0f76a182
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-19 19:14:13 +0000

    Update orders.py

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -8,4 +8,6 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('',
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Impor
