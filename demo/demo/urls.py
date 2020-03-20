fb937e775414ee520cf2d3f023cca11c9f46a905
girlsways <109319858+girlsways@users.noreply.github.com>
2020-03-20 05:25:46 +0000

    Added 7 tweets

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -10,4 +10,4 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Impor
+    1. Import the include() function: from django.urls import inclu
