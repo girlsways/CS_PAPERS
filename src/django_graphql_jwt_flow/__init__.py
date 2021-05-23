9be31fafbe29f15447d5c75aebd3691036301ffa
girlsways <109319858+girlsways@users.noreply.github.com>
2021-05-23 11:02:25 +0000

    fix podspec

--- src/django_graphql_jwt_flow/__init__.py
+++ src/django_graphql_jwt_flow/__init__.py
@@ -1,3 +1,8 @@
 from collections import namedtuple
 
-semantic = namedtuple("semantic", ["major", 
+semantic = namedtuple("semantic", ["major", "minor", "patch"])
+__VERSION__ = "0.0.0"
+
+
+def get_version():
+    def
