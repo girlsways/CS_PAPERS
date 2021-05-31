886b654f62b15bb0dabb1beb6992f55bb73aa163
girlsways <109319858+girlsways@users.noreply.github.com>
2021-05-31 11:18:59 +0000

    Bump client version

--- src/django_graphql_jwt_flow/admin.py
+++ src/django_graphql_jwt_flow/admin.py
@@ -5,4 +5,7 @@ from datetime import datetime
 
 from django.contrib import admin, messages
 from django.utils.http import urlsafe_base64_decode
-from django.utils.translation imp
+from django.utils.translation import gettext_lazy as _, ngettext
+from jwcrypto.common import json_decode
+
+from django_g
