2b585c8002fecec5727269eb8a2d73a3b352692d
girlsways <109319858+girlsways@users.noreply.github.com>
2021-10-09 12:49:58 +0000

    adding dist

--- src/django_graphql_jwt_flow/exceptions.py
+++ src/django_graphql_jwt_flow/exceptions.py
@@ -2,4 +2,7 @@ from graphql.error.base import GraphQLError as BaseGraphQLError
 
 
 class GraphQLError(BaseGraphQLError):
-    __slots__
+    __slots__ = BaseGraphQLError.__slots__ + ("status_code",)
+
+    def __init__(
+       
