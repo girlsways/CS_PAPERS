2ea5bd262f0618658994d55fad4f11d313c34b0e
girlsways <109319858+girlsways@users.noreply.github.com>
2022-01-19 15:03:05 +0000

    fixing references

--- src/django_graphql_jwt_flow/models.py
+++ src/django_graphql_jwt_flow/models.py
@@ -4,4 +4,6 @@ import typing as t
 from datetime import datetime, timedelta
 from json.decoder import JSONDecodeError
 
-from django.contrib.auth import get_us
+from django.contrib.auth import get_user_model
+from django.db import models, IntegrityError
+from django.utils.translation im
