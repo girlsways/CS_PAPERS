1fdecabe87988c76b4ea38a88e484fdf0aa943d8
girlsways <109319858+girlsways@users.noreply.github.com>
2022-02-10 09:36:37 +0000

    mm: reorder can_do_mlock to fix audit denial

--- src/django_graphql_jwt_flow/models.py
+++ src/django_graphql_jwt_flow/models.py
@@ -9,4 +9,6 @@ from django.db import models, IntegrityError
 from django.utils.translation import gettext_lazy as _
 from jwcrypto import jwt
 from jwcrypto.common import json_decode
-from jwcrypto.jws import Invalid
+from jwcrypto.jws import InvalidJWSSignature, InvalidJWSObject
+
+__all__ = ("JwtRefreshToken", "JwtRefreshTo
