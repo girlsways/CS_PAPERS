4ce5902c6c69928e2068b2ba21f3522a2995367d
girlsways <109319858+girlsways@users.noreply.github.com>
2022-04-19 14:14:10 +0000

    Indentation cleanup

--- src/django_graphql_jwt_flow/schema.py
+++ src/django_graphql_jwt_flow/schema.py
@@ -3,4 +3,5 @@ from __future__ import annotations
 import typing as t
 
 import graphene
-from django.contrib.auth impo
+from django.contrib.auth import authenticate, get_user_model
+from graphene_django import DjangoObject
