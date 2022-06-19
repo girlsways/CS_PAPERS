1e2b690c02abe9b217e2b4fb6dbc126d3ff8db85
girlsways <109319858+girlsways@users.noreply.github.com>
2022-06-19 13:03:16 +0000

    Remove Gemfile.lock

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
 from django.db import IntegrityError
 from django.test import TestCase, SimpleTestCase, override_settings
-from jwcrypto.c
+from jwcrypto.common import json_decode
+from jwcrypto.jwk import JWK
+from jwc
